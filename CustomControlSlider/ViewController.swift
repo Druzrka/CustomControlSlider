3167ba239c3f276206b5c3d793bde8c70a85a32b
Zakhar <markwill@MacBook-Pro-Maran.local>
2018-11-19 12:48:51 +0200

    Initial commit

---
+++ CustomControlSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomControlSlider
+//
+//  Created by Zakhar on 11/19/18.
+//  Copyright © 2018 Zakhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
